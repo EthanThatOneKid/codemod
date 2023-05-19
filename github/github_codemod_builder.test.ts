0f59fd0c8c29cfd65be5ce06554bf087ca54b0fb
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2023-05-18 17:51:59 -0700

    Add initial test file

---
+++ github/github_codemod_builder.test.ts
@@ -0,0 +1,7 @@
+import { assertEquals } from "../dev_deps.ts";
+
+Deno.test("GitHubCodemodBuilder creates a tree", () => {
+  // TODO(https://oss.acmcsuf.com/codemod/pull/3#pullrequestreview-1433626635):
+  // Write unit tests to guarantee the behavior covering the entire module.
+  assertEquals(1, 1);
+});
