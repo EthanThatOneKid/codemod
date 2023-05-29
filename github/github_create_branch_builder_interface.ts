1ced556503b2bd18acf8bdb7a60b75c9d26407d8
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2023-05-28 23:23:56 -0700

    Design `github_create_branch_builder_interface.ts`

--- github/github_create_branch_builder_interface.ts
+++ github/github_create_branch_builder_interface.ts
@@ -1,12 +1,30 @@
 import type { GitHubAPIRefsPostRequest } from "./api/mod.ts";
+import type { Generate } from "./types.ts";
 
 /**
  * GitHubCreateBranchBuilderInterface is an interface for a
  * GitHubCreateBranchBuilder.
+ *
+ * @see https://docs.github.com/en/rest/reference/git#create-a-reference
  */
 export interface GitHubCreateBranchBuilderInterface {
   /**
    * run executes the builder.
    */
-  run(): GitHubAPIRefsPostRequest;
+  run(): Promise<GitHubAPIRefsPostRequest>;
+
+  /**
+   * clone clones the builder.
+   */
+  clone(): this;
+
+  /**
+   * ref sets the ref.
+   */
+  ref(refOrRefGenerate: Generate<string, []>): this;
+
+  /**
+   * sha sets the commit or tree SHA.
+   */
+  sha(shaOrSHAGenerate: Generate<string, []>): this;
 }
