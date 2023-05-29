925401108f9e225d7c61448ecfbbd4461415e86a
Ethan Davidson <31261035+EthanThatOneKid@users.noreply.github.com>
2023-05-28 23:35:42 -0700

    Design `github_update_branch_builder_interface.ts`.

--- github/github_update_branch_builder_interface.ts
+++ github/github_update_branch_builder_interface.ts
@@ -1,13 +1,35 @@
 import type { GitHubAPIRefPatchRequest } from "./api/mod.ts";
+import type { Generate } from "./types.ts";
 
 /**
- * GitHubCodemodUpdateBranchBuilderInterface is an interface for a GitHubCodemodUpdateBranchBuilder.
+ * GitHubUpdateBranchBuilderInterface is an interface for a
+ * GitHubUpdateBranchBuilder.
  *
  * @see https://docs.github.com/en/rest/reference/git#update-a-reference
  */
-export interface GitHubCodemodUpdateBranchBuilderInterface {
+export interface GitHubUpdateBranchBuilderInterface {
   /**
    * run executes the builder.
    */
-  run(): GitHubAPIRefPatchRequest;
+  run(): Promise<GitHubAPIRefPatchRequest>;
+
+  /**
+   * clone clones the builder.
+   */
+  clone(): this;
+
+  /**
+   * sha sets the commit or tree SHA.
+   */
+  sha(shaOrSHAGenerate: Generate<string, []>): this;
+
+  /**
+   * ref sets the ref.
+   */
+  ref(refOrRefGenerate: Generate<string, []>): this;
+
+  /**
+   * force sets the force flag. If empty, the force flag is set to true.
+   */
+  force(forceOrForceGenerate?: Generate<boolean, []>): this;
 }
